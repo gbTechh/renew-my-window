e8f9a6ae355b0171f8b725ffbca7192a47516701
Rodrigo <8kdots@gmail.com>
2023-04-06 00:59:57 -0500

    add other image animation

--- js/app.js
+++ js/app.js
@@ -1,126 +1,117 @@
-
-
 const image = document.getElementById("js-image-movible-box");
+const image2 = document.getElementById("js-image-movible-box-2");
 const imageHidden = document.getElementById("js-image-movible-hidden");
+const widthImage = image.offsetWidth;
+const widthImage2 = image.offsetWidth;
+const heightImage = image2.offsetHeight;
+const heightImage2 = image2.offsetHeight;
 
 const boxEnd = document.getElementById("js-box-end");
+const boxEnd2 = document.getElementById("js-box-end-2");
+boxEnd.style.opacity = '0';
+boxEnd2.style.opacity = '0';
 const widthBox = boxEnd.offsetWidth;
 const heightBox = boxEnd.offsetHeight;
-console.log({heightBox});
+const widthBox2 = boxEnd2.offsetWidth;
+const heightBox2 = boxEnd2.offsetHeight;
+
 
 // console.log(image)
 
 if (image) {
-  const imageWidth = image.offsetWidth;
-  const imageHeight = image.offsetHeight;
-  // console.log(imageHeight)0px 20px 30px
-
-  // const boxDistanciaFromTop = boxEnd.getBoundingClientRect().top;
-  const distanciaImgFromTop = image.offsetTop + imageHeight;
-  console.log(boxEnd.getBoundingClientRect())
-  // console.log({ imageWidth })
-  const options = {
-    rootMargin: '0px',
-    threshold: 1.0
-  };
 
   window.addEventListener("scroll", () => {
     const scrollTop = window.scrollTop || document.documentElement.scrollTop;
 
     const rectBox = boxEnd.getBoundingClientRect();
+    const rectBox2 = boxEnd2.getBoundingClientRect();
     const rectImage = imageHidden.getBoundingClientRect();
     const boxDistanciaFromTop = rectBox.top + window.scrollY
+    const boxDistanciaFromTop2 = rectBox2.top + window.scrollY
     const imgDistanciaFromTop = rectImage.top + window.scrollY
+    const imgDistanciaFromTop2 = rectImage.top + window.scrollY
     // console.log(window.innerHeight)
 
     const initPoint = imgDistanciaFromTop - (window.innerHeight / 2)
+    const initPoint2 = imgDistanciaFromTop2 - (window.innerHeight / 2)
     const endPoint = boxDistanciaFromTop - (window.innerHeight / 2)
+    const endPoint2 = boxDistanciaFromTop2 - (window.innerHeight / 2)
     const totalRecorrido = endPoint - initPoint
+    const totalRecorrido2 = endPoint2 - initPoint2
     let movement = (scrollTop - initPoint)
+    let movement2 = (scrollTop - initPoint2)
     let percent = movement * 100 / totalRecorrido
-    // console.log({percent})
-    if(percent > 50){
-      image.style.trasnform = `translateX(${movement}px) `;
+    let percent2 = movement2 * 100 / totalRecorrido2
+
+    if (percent > 50) {
+      image.style.transform = `translateX(${movement}px) `;
+      image2.style.transform = `translateX(${movement2}px) `;
     }
 
-    if(scrollTop > initPoint){
+    if (scrollTop > initPoint) {
       image.style.opacity = '1'
-      
-      if(percent <= 20){
-        image.style.borderRadius = `${percent}px`
+      image2.style.opacity = '1'
+
+      if (percent < 5) {
+        image.style.opacity = '0'
+        image2.style.opacity = '0'
+        image.style.transition = '0.3s opacity ease'
+        image2.style.transition = '0.3s opacity ease'
       }
-      if(percent <= 70){
-        console.log({percent})
+      if (percent <= 20) {
+        image.style.borderRadius = `${percent}px`
+        image2.style.borderRadius = `${percent2}px`
+      }
+      if (percent <= 70) {
         image.style.transform = `translateY(${movement}px) translateX(-${percent}vw) rotate3d(1,0,0,${percent}deg)`;
-      }else if(percent <= 90){
+        image2.style.transform = `translateY(${movement2}px) translateX(${percent2}vw) rotate3d(1,0,0,-${percent2}deg)`;
+      } else if (percent <= 98) {
         image.style.transform = `translateY(${movement}px) translateX(-${100 - percent}vw)  rotate3d(0,0,1,${percent}deg)`;
-      }else{
-        image.style.transform = `translateY(${movement}px) translateX(-${100 - percent}vw)`;
+        image2.style.transform = `translateY(${movement2}px) translateX(${100 - percent2}vw)  rotate3d(0,0,1,-${percent2}deg)`;
+        image.style.maxWidth = `${widthImage}px`
+        image2.style.maxWidth = `${widthImage2}px`
+        image.style.maxHeight = `${heightImage}px`
+        image2.style.maxHeight = `${heightImage2}px`
+        boxEnd.style.opacity = '0'
+        boxEnd2.style.opacity = '0'
+        boxEnd.style.transition = '0.3s opacity ease-in-out'
+        boxEnd2.style.transition = '0.3s opacity ease-in-out'
+      } else {
         image.style.width = `${widthBox}px`
-        image.style.trasnsition = `0.5s width ease-in`
+        image2.style.width = `${widthBox2}px`
+        image.style.maxWidth = `${widthBox}px`
+        image2.style.maxWidth = `${widthBox2}px`
+        image.style.maxHeight = `${heightBox}px`
+        image2.style.maxHeight = `${heightBox2}px`
+        image.style.transition = `0.5s max-width ease `
+        image2.style.transition = `0.5s max-width ease `
+        image.style.opacity = '0'
+        image.style.transition = '0.1s opacity ease-in-out'
+        image2.style.opacity = '0'
+        image2.style.transition = '0.1s opacity ease-in-out'
+        let heightImage = image.offsetHeight;
+        let heightImage2 = image2.offsetHeight;
+        image.style.transform = `translateY(${movement + (heightImage / 3)}px) translateX(-${110 - percent}vw)`;
+        image2.style.transform = `translateY(${movement2 + (heightImage2 / 3)}px) translateX(${110 - percent2}vw)`;
+
+        boxEnd.style.opacity = '1'
+        boxEnd2.style.opacity = '1'
+        boxEnd.style.transition = '1s opacity ease-in-out'
+        boxEnd2.style.transition = '1s opacity ease-in-out'
+
       }
       // image.style.transform = `tr`
       // console.log({percent})
-    }else{
-      image.style.oapcity = '0'
-    }
-    if(scrollTop >= endPoint) {
+    } else {
       image.style.opacity = '0'
-      console.log('end')
+      image2.style.opacity = '0'
     }
-    // console.log({scrollTop})
+    if (scrollTop >= endPoint) {
+      image.style.opacity = '0'
+      image2.style.opacity = '0'
+
+    }
   })
- 
-
-  // const observer = new IntersectionObserver(entries => {
-  //   let scrollPixeles = 0
-  //   let startPoint = imageHeight
-  //   let dis = 0
-  //   const windowWidth = window.innerWidth;
-    
-
-  //   // window.addEventListener("scroll", () => {
-  //   //   let distanciaTotalARecorrer = boxDistanciaFromTop - distanciaImgFromTop;
-  //   //   // console.log({distanciaTotalARecorrer})
-  //   //   // console.log({boxDistanciaFromTop})
-  //   //   const scrollTop = window.scrollTop || document.documentElement.scrollTop;
-  //   //   // console.log(scrollTop)
-  //   // },{once:true})
-
-  //   if (entries[0].isIntersecting) {
-  //     console.log('El elemento está dentro de la vista');
-      
-  //     // window.addEventListener("scroll", () => {
-  //     //   const scrollTop = window.scrollTop || document.documentElement.scrollTop;
-  //     //   scrollPixeles = scrollTop - startPoint;
-  //     //   movementLeft = scrollTop - 300;
-  //     //   // console.log({ distanciaAlTop })
-  //     //   // console.log({ dis })
-  //     //   // console.log({ scrollTop })
-  //     //   dis = distanciaAlTop - scrollTop
-  //     //   // console.log(dis >= (distanciaAlTop - 10))
-  //     //   image.style.opacity = '1'
-  //     //   if (dis >= (distanciaAlTop - 10)) {
-  //     //     image.style.opacity = '0'
-  //     //   }
-  //     //   if ((scrollPixeles) > (windowWidth / 2)) {
-  //     //     scrollPixeles = scrollPixeles
-  //     //   }
-  //     //   image.style.transform = `translateY(${scrollTop}px) translateX(-${scrollPixeles * 0.05}px) `;
-
-  //     // });
-
-  //   } else {
-  //     console.log('El elemento está fuera de la vista');
-  //     image.style.transform = `translateY(${scroll}px)`;
-  //     image.style.opacity = '0'
-  //   }
-  // }, options);
-
-  // if (image) {
-  //   observer.observe(image);
-  // }
-
 }
 
 
